75c15c8b353f23fea4bf387fb2b88de08b279fd8
Jimel J. Joseph <jimelj@gmail.com>
2017-05-15 22:01:19 -0400

    fixing db

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,23 @@
-var mysql = require("mysql");
+/*jshint esversion:6*/
+let mysql = require("mysql");
+let connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: process.env.PORT || 3306,
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+  connection = mysql.createConnection({
+   host: "localhost",
+   port: process.env.PORT || 3306,
+   user: "root",
+   password: "",
+   database: "burgers_db"
+ });
+
+}
+
+
+
+
 
 connection.connect(function(err) {
   if (err) {
